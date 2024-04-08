08fc079855cb5c59d545c814019b2ebf22e2935e
maximofn <maximofn@gmail.com>
2024-04-08 08:20:49 +0200

    Add localization support to autostart file

--- add_to_startup.sh
+++ add_to_startup.sh
@@ -18,18 +18,19 @@ fi
 # Variables
 script_name="ram_monitor.sh"
 script_info="RAM Monitor"
+languaje=$(locale | grep LANG= | cut -d= -f2 | cut -d. -f1)
 
 # Create autostart file
 touch $AUTOSTART_PATH/$script_name.desktop
 
 # Write autostart file
-echo "[Desktop Entry]" > $AUTOSTART_PATH/cpu_monitor_test.sh.desktop
-echo "Type=Application" >> $AUTOSTART_PATH/cpu_monitor_test.sh.desktop
-echo "Exec=$SCRIPT_PATH/$script_name" >> $AUTOSTART_PATH/cpu_monitor_test.sh.desktop
-echo "Hidden=false" >> $AUTOSTART_PATH/cpu_monitor_test.sh.desktop
-echo "NoDisplay=false" >> $AUTOSTART_PATH/cpu_monitor_test.sh.desktop
-echo "X-GNOME-Autostart-enabled=true" >> $AUTOSTART_PATH/cpu_monitor_test.sh.desktop
-echo "Name[es_ES]=$script_info" >> $AUTOSTART_PATH/cpu_monitor_test.sh.desktop
-echo "Name=$script_info" >> $AUTOSTART_PATH/cpu_monitor_test.sh.desktop
-echo "Comment[es_ES]=$script_info" >> $AUTOSTART_PATH/cpu_monitor_test.sh.desktop
-echo "Comment=$script_info" >> $AUTOSTART_PATH/cpu_monitor_test.sh.desktop
+echo "[Desktop Entry]" > $AUTOSTART_PATH/$script_name.desktop
+echo "Type=Application" >> $AUTOSTART_PATH/$script_name.desktop
+echo "Exec=$SCRIPT_PATH/$script_name" >> $AUTOSTART_PATH/$script_name.desktop
+echo "Hidden=false" >> $AUTOSTART_PATH/$script_name.desktop
+echo "NoDisplay=false" >> $AUTOSTART_PATH/$script_name.desktop
+echo "X-GNOME-Autostart-enabled=true" >> $AUTOSTART_PATH/$script_name.desktop
+echo "Name[$languaje]=$script_info" >> $AUTOSTART_PATH/$script_name.desktop
+echo "Name=$script_info" >> $AUTOSTART_PATH/$script_name.desktop
+echo "Comment[$languaje]=$script_info" >> $AUTOSTART_PATH/$script_name.desktop
+echo "Comment=$script_info" >> $AUTOSTART_PATH/$script_name.desktop
