91e4c6288e4b80dbe846549af31aea8f38c1c718
maximofn <maximofn@gmail.com>
2024-05-03 09:19:42 +0200

    Create logs files

--- ram_monitor.sh
+++ ram_monitor.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # Get script path
 SCRIPT_PATH=$(dirname $0)
-/usr/bin/python3 $SCRIPT_PATH/ram_monitor.py
+/usr/bin/python3 $SCRIPT_PATH/ram_monitor.py >ram_monitor.log 2>ram_monitor_error.log
